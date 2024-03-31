727e685256e757b7002b9cc54e5bb6f74251ad14
gyaneswarpritam <gyaneswar.pritam@gmail.com>
2024-03-31 22:37:05 +0530

    changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     jwtSecret: 'secret-pritam',
-    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/intecfair',
+    mongoURI: process.env.MONGO_URI,
+    // mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/intecfair',
     // twilioAccountSid: 'your-account-sid',
     // twilioAuthToken: 'your-auth-token',
     // twilioPhoneNumber: 'your-twilio-phone-number'
